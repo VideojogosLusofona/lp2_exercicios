93c1df3b754f176cff3d15bdb466207ec7aee54f
FPTheFluffyPawed <n.ito10@hotmail.com>
2019-11-23 16:29:31 +0000

    tentativa solução 2

---
+++ solucoes/02/013_sol1/LP2Testing/Assets/ex02_13/ReadInput.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadInput : MonoBehaviour
+{
+    public Vector2 GetVector2()
+    {
+        return new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+    }
+}
---
+++ solucoes/02/013_sol1/LP2Testing/Assets/ex02_13/ShowText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowText : MonoBehaviour
+{
+    // Get the Text Box to use.
+    [SerializeField]
+    Text textBox;
+    ReadInput readInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        readInput = gameObject.AddComponent<ReadInput>() as ReadInput;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textBox.text = readInput.GetVector2().ToString();
+    }
+}
